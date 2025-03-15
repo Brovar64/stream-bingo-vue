6393dc59bfebd1b5a1f31c296af7a78c741a2096
Brovar64 <brovar64@gmail.com>
2025-03-15 15:26:23 +0100

    Fix CSS import path in main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import './assets/index.css'
+import './assets/css/tailwind.css'
 
 // VueFire
 import { VueFire, VueFireAuth } from 'vuefire'
